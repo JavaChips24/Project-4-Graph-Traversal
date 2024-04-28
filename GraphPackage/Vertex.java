26080a912ce7b43abb37ad9f7da3adb211ee7673
Ashley Mapes <amapes05@gmail.com>
2024-04-27 23:15:38 -0400

    added NeighborIterator

--- GraphPackage/Vertex.java
+++ GraphPackage/Vertex.java
@@ -6,55 +6,86 @@ import ADTPackage.List.LinkedListWithIterator;
 import ADTPackage.List.ListWithIteratorInterface;
 /**
  A class of vertices for a graph.
- @author Frank M. Carrano
- @author Timothy M. Henry
- @version 5.0
- */
+@author Frank M. Carrano
+@author Timothy M. Henry
+@version 5.0
+*/
 class Vertex<T> implements VertexInterface<T>
-{
-   private T label;
-   private ListWithIteratorInterface<Edge> edgeList; // Edges to neighbors
-   private boolean visited;                          // True if visited
-   private VertexInterface<T> previousVertex;        // On path to this vertex
-   private double cost;                              // Of path to this vertex
-   
-   public Vertex(T vertexLabel)
-   {
-      label = vertexLabel;
-      edgeList = new LinkedListWithIterator<>();
-      visited = false;
-      previousVertex = null;
-      cost = 0;
-   } // end constructor
+    {
+    private T label;
+    private ListWithIteratorInterface<Edge> edgeList; // Edges to neighbors
+    private boolean visited;                          // True if visited
+    private VertexInterface<T> previousVertex;        // On path to this vertex
+    private double cost;                              // Of path to this vertex
 
-/* Implementations of the vertex operations go here.
-   . . . */
+    public Vertex(T vertexLabel)
+    {
+        label = vertexLabel;
+        edgeList = new LinkedListWithIterator<>();
+        visited = false;
+        previousVertex = null;
+        cost = 0;
+    } // end constructor
 
-   protected class Edge
-   {
-      private VertexInterface<T> vertex; // Vertex at end of edge
-      private double weight;
-      
-      protected Edge(VertexInterface<T> endVertex, double edgeWeight)
-      {
-         vertex = endVertex;
-         weight = edgeWeight;
-      } // end constructor
-      
-      protected Edge(VertexInterface<T> endVertex)
-      {
-         vertex = endVertex;
-         weight = 0;
-      } // end constructor
+    /* Implementations of the vertex operations go here.
+    . . . */
+    
 
-      protected VertexInterface<T> getEndVertex()
-      {
-         return vertex;
-      } // end getEndVertex
-      
-      protected double getWeight()
-      {
-         return weight; 
-      } // end getWeight
-   } // end Edge
+
+
+
+    private class NeighborIterator implements Iterator<VertexInterface<T>>
+    {
+        private Iterator<Edge> edges;
+        private NeighborIterator()
+        {
+            edges = edgeList.getIterator();
+        } // end default constructor
+        public boolean hasNext()
+        {
+            return edges.hasNext();
+        } // end hasNext
+        public VertexInterface<T> next()
+        {
+            VertexInterface<T> nextNeighbor = null;
+            if (edges.hasNext()){
+                Edge edgeToNextNeighbor = edges.next();
+                nextNeighbor = edgeToNextNeighbor.getEndVertex();
+            }
+            else
+                throw new NoSuchElementException();
+            return nextNeighbor;
+        } // end next
+        public void remove()
+        {
+            throw new UnsupportedOperationException();
+        }
+    }
+    protected class Edge
+    {
+        private VertexInterface<T> vertex; // Vertex at end of edge
+        private double weight;
+
+        protected Edge(VertexInterface<T> endVertex, double edgeWeight)
+        {
+            vertex = endVertex;
+            weight = edgeWeight;
+        } // end constructor
+
+        protected Edge(VertexInterface<T> endVertex)
+        {
+            vertex = endVertex;
+            weight = 0;
+        } // end constructor
+
+        protected VertexInterface<T> getEndVertex()
+        {
+            return vertex;
+        } // end getEndVertex
+
+        protected double getWeight()
+        {
+            return weight; 
+        } // end getWeight
+    } // end Edge
 } // end Vertex
