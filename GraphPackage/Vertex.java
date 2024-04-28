e1b190395bc4b3bc9e1b860b1c892816c6c907d1
Ashley Mapes <amapes05@gmail.com>
2024-04-27 23:40:45 -0400

    added dictionary interface

--- GraphPackage/Vertex.java
+++ GraphPackage/Vertex.java
@@ -37,47 +37,78 @@ class Vertex<T> implements VertexInterface<T>
 
     public void visit()
     {
-
+        visited = true;
     } // end visit
  
     public void unvisit()
     {
-        
+        visited = false;   
     } // end unvisit
 
     public boolean isVisited()
     {
-
+        return visited;
     } // end isVisited
- 
+
     public boolean connect(VertexInterface<T> endVertex, double edgeWeight)
     {
-
+       boolean result = false;
+    
+       if (!this.equals(endVertex))
+       {  // Vertices are distinct
+          Iterator<VertexInterface<T>> neighbors = getNeighborIterator();
+          boolean duplicateEdge = false;
+    
+          while (!duplicateEdge && neighbors.hasNext())
+          {
+             VertexInterface<T> nextNeighbor = neighbors.next();
+             if (endVertex.equals(nextNeighbor))
+                duplicateEdge = true;
+          } // end while
+    
+          if (!duplicateEdge)
+          {
+             edgeList.add(new Edge(endVertex, edgeWeight));
+             result = true;
+          } // end if
+       } // end if
+    
+       return result;
     } // end connect
-                          
+    
     public boolean connect(VertexInterface<T> endVertex)
     {
-
+       return connect(endVertex, 0);
     } // end connect
 
     public Iterator<VertexInterface<T>> getNeighborIterator()
     {
-
+        return new NeighborIterator();
     } // end getNeighborIterator
 
     public Iterator<Double> getWeightIterator()
     {
-
+        
     } // end getWeightIterator
  
     public boolean hasNeighbor()
     {
-
+       return !edgeList.isEmpty();
     } // end hasNeighbor
  
     public VertexInterface<T> getUnvisitedNeighbor()
     {
-
+       VertexInterface<T> result = null;
+    
+       Iterator<VertexInterface<T>> neighbors = getNeighborIterator();
+       while ( neighbors.hasNext() && (result == null) )
+       {
+          VertexInterface<T> nextNeighbor = neighbors.next();
+          if (!nextNeighbor.isVisited())
+             result = nextNeighbor;
+       } // end while
+    
+       return result;
     } // end getUnvisitedNeighbor
  
     public void setPredecessor(VertexInterface<T> predecessor)
@@ -94,6 +125,19 @@ class Vertex<T> implements VertexInterface<T>
     {
 
     } // end hasPredecessor
+
+    public boolean equals(Object other)
+    {
+        boolean result;
+        if((other == null) || (getClass() != other.getClass()))
+            result = false;
+        else{
+            @SuppressWarnings("unchecked")
+            Vertex<T> otherVertex = (Vertex<T>)other;
+            result = label.equals(otherVertex.label);
+        }
+        return result;
+    } // end equals
  
     public void setCost(double newCost)
     {
