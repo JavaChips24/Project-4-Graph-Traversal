809f1bbf3c9ef072a8a2cb2f48b7956dc184cade
Ashley Mapes <amapes05@gmail.com>
2024-04-28 22:35:45 -0700

    added more methods and a resizable arraystack ADT

--- ADTPackage/GraphPackage/Graph.java
+++ ADTPackage/GraphPackage/Graph.java
@@ -83,13 +83,94 @@ public final class Graph<T> implements GraphInterface<T> {
         } // end while
     } // end reset Vertices
 
-   public QueueInterface<T> getBreadthFirstTraversal(T origin);
+    public QueueInterface<T> getBreadthFirstTraversal(T origin) {
+        resetVertices();
+        QueueInterface<T> traversalOrder = new LinkedQueue<>();
+        QueueInterface<VertexInterface<T>> vertexQueue = new LinkedQueue<>();
+        
+        // Get the origin vertex and mark it as visited
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        
+        // Enqueue the origin vertex
+        vertexQueue.enqueue(originVertex);
+    
+        // Perform breadth-first traversal
+        while (!vertexQueue.isEmpty()) {
+            VertexInterface<T> currentVertex = vertexQueue.dequeue();
+            traversalOrder.enqueue(currentVertex.getLabel()); // Enqueue vertex label
+    
+            // Visit all unvisited neighbors of the current vertex
+            Iterator<VertexInterface<T>> neighborIterator = currentVertex.getNeighborIterator();
+            while (neighborIterator.hasNext()) {
+                VertexInterface<T> neighbor = neighborIterator.next();
+                if (!neighbor.isVisited()) {
+                    neighbor.visit();
+                    vertexQueue.enqueue(neighbor);
+                }
+            }
+        }
+        
+        return traversalOrder;
+    } // end getBreadthFirstTraversal
 
-   public QueueInterface<T> getDepthFirstTraversal(T origin);
+    public QueueInterface<T> getDepthFirstTraversal(T origin) {
+        // Reset the visited status of all vertices
+        resetVertices();
+        
+        // Queue to store the traversal order
+        QueueInterface<T> traversalOrder = new LinkedQueue<>();
+        
+        // Stack to perform depth-first traversal
+        StackInterface<VertexInterface<T>> vertexStack = new ResizableArrayStack<>();
+        
+        // Get the vertex corresponding to the origin value and mark it as visited
+        VertexInterface<T> originVertex = vertices.getValue(origin);
+        originVertex.visit();
+        
+        // Enqueue the origin vertex label into the traversal order queue
+        traversalOrder.enqueue(origin); // Enqueue vertex
+        
+        // Push the origin vertex onto the stack
+        vertexStack.push(originVertex); // Push vertex
+        
+        // Perform depth-first traversal
+        while (!vertexStack.isEmpty()) {
+            // Peek the top vertex from the stack
+            VertexInterface<T> topVertex = vertexStack.peek();
+            
+            // Get an unvisited neighbor of the top vertex
+            VertexInterface<T> nextNeighbor = topVertex.getUnvisitedNeighbor();
+                
+            if (nextNeighbor != null) {
+                // If an unvisited neighbor is found, mark it as visited,
+                // enqueue its label, and push it onto the stack
+                nextNeighbor.visit();
+                traversalOrder.enqueue(nextNeighbor.getLabel());
+                vertexStack.push(nextNeighbor);
+            } else {
+                // If no unvisited neighbor is found, pop the top vertex
+                // from the stack
+                vertexStack.pop();
+            }
+        }
+        
+        // Return the traversal order queue
+        return traversalOrder;
+    }
 
-   public StackInterface<T> getTopologicalOrder();
+   public StackInterface<T> getTopologicalOrder()
+   {
 
-   public int getShortestPath(T begin, T end, StackInterface<T> path);
+   }
 
-   public double getCheapestPath(T begin, T end, StackInterface<T> path);
+   public int getShortestPath(T begin, T end, StackInterface<T> path)
+   {
+
+   }
+
+   public double getCheapestPath(T begin, T end, StackInterface<T> path)
+   {
+
+   }
 }
